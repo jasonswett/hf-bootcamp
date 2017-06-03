9fbf571f823b69ac492006d81708fab4a9accf64
Jason Swett <jason@benfranklinlabs.com>
2017-06-03 15:33:57 -0400

    Add ability to add restauarants.

---
+++ week-2/labs/order_genius_with_adding/order_genius.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require_relative './ui.rb'
+require_relative './tag.rb'
+require_relative './restaurant.rb'
+
+ui = UI.new
+
+if ARGV[0] == 'tags' || ARGV[0].nil?
+  ui.list_tags
+  exit
+end
+
+if ARGV[0] == 'restaurants'
+  ui.list_restaurants(Restaurant.all)
+  exit
+end
+
+if ARGV[0] == 'add_restaurant'
+  Restaurant.create(
+    name: ARGV[1],
+    phone: ARGV[2]
+  )
+
+  exit
+end
+
+unless Tag.find_by_name(ARGV[0]).any?
+  ui.exit_with_message "No such tag \"#{ARGV[0]}\""
+end
+
+ui.list_restaurants_with_tag(ARGV[0])
